00e4b2b51eb5bf3876f1168a036cab1c8c29a2ba
bangbang93 <bangbang93@163.com>
2017-10-02 22:34:31 +0800

    use async

--- routes/router.js
+++ routes/router.js
@@ -2,80 +2,70 @@
  * Created by bangbang93 on 16/9/12.
  */
 'use strict';
-const router = require('express').Router();
+const router = require('express-promise-router');
 const request = require('co-request');
-const co = require('co');
-const fs = require('co-fs');
+const fs = require('fs-extra');
 const path = require('path');
 const mkdirp = require('co-mkdirp');
 const publicDir = path.resolve(__dirname, '../public');
 const helper = require('../helper/replace');
 
-router.get('/css', function (req, res, next) {
+router.get('/css', async function (req, res) {
   let url = req.url;
   let https = Boolean(req.get('isHttps'));
   let file = path.join(publicDir, `css/${https?'https':'http'}/${encodeURIComponent(url.substr(5))}`);
-  co(function*(){
-    if (yield fs.exists(file)){
-      res.type('css').sendFile(file);
-    } else {
-      let response = yield request.get('https://fonts.googleapis.com' + url);
-      if (response.statusCode != 200){
-        return res.status(response.statusCode).send(response.body);
-      }
-      let style = helper.replaceBody(response.body);
-      res.type('css').send(style);
-      if (! (yield fs.exists(path.dirname(file)))){
-        yield mkdirp(path.dirname(file));
-      }
-      yield fs.writeFile(file, style);
+  if (await fs.exists(file)){
+    res.type('css').sendFile(file);
+  } else {
+    let response = await request.get('https://fonts.googleapis.com' + url);
+    if (response.statusCode !== 200){
+      return res.status(response.statusCode).send(response.body);
     }
-  })
-    .catch(next);
+    let style = helper.replaceBody(response.body);
+    res.type('css').send(style);
+    if (! (await fs.exists(path.dirname(file)))){
+      await mkdirp(path.dirname(file));
+    }
+    await fs.writeFile(file, style);
+  }
 });
 
-router.get('/s/*', function (req, res, next) {
+router.get('/s/*', async function (req, res) {
   let url = req.url;
   let file = path.join(publicDir, url);
-  co(function*(){
-    if (yield fs.exists(file)){
+    if (await fs.exists(file)){
       res.type('font').sendFile(file);
     } else {
-      let response = yield request.get('https://fonts.gstatic.com' + url, {encoding: null});
-      if (response.statusCode != 200){
+      let response = await request.get('https://fonts.gstatic.com' + url, {encoding: null});
+      if (response.statusCode !== 200){
         return res.status(response.statusCode).send(response.body);
       }
       let font = response.body;
       res.type('font').send(font);
-      if (! (yield fs.exists(path.dirname(file)))){
-        yield mkdirp(path.dirname(file));
+      if (! (await fs.exists(path.dirname(file)))){
+        await mkdirp(path.dirname(file));
       }
-      yield fs.writeFile(file, font);
+      await fs.writeFile(file, font);
     }
-  })
-    .catch(next);
 });
 
-router.get('/ajax/*', function (req, res, next) {
+router.get('/ajax/*', async function (req, res) {
   let url = req.url;
   let file = path.join(publicDir, url);
-  co(function*(){
-    if (yield fs.exists(file)){
-      res.type('js').sendFile(file);
-    } else {
-      let response = yield request.get('https://ajax.googleapis.com' + url);
-      if (response.statusCode != 200){
-        return res.status(response.statusCode).send(response.body);
-      }
-      let font = response.body;
-      res.type('js').send(font);
-      if (! (yield fs.exists(path.dirname(file)))){
-        yield mkdirp(path.dirname(file));
-      }
-      yield fs.writeFile(file, font);
+  if (await fs.exists(file)){
+    res.type('js').sendFile(file);
+  } else {
+    let response = await request.get('https://ajax.googleapis.com' + url);
+    if (response.statusCode !== 200){
+      return res.status(response.statusCode).send(response.body);
     }
-  })
-    .catch(next);
+    let font = response.body;
+    res.type('js').send(font);
+    if (! (await fs.exists(path.dirname(file)))){
+      await mkdirp(path.dirname(file));
+    }
+    await fs.writeFile(file, font);
+  }
 });
 
 module.exports = router;
