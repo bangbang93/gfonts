42d3a82e56a43ae09d55707d2c1f650725cf5d25
bangbang93 <bangbang93@163.com>
2018-04-27 10:03:14 +0800

    upgrade dependencies

--- routes/router.js
+++ routes/router.js
@@ -3,10 +3,9 @@
  */
 'use strict';
 const router = require('express-promise-router')();
-const request = require('co-request');
+const request = require('request-promise');
 const fs = require('fs-extra');
 const path = require('path');
-const mkdirp = require('co-mkdirp');
 const publicDir = path.resolve(__dirname, '../public');
 const ReplaceHelper = require('../helper/replace');
 const HashHelper = require('../helper/hash')
@@ -16,57 +15,54 @@ router.get('/css', async function (req, res) {
   let url = req.url;
   let https = Boolean(req.get('isHttps'));
   let file = path.join(publicDir, `css/${https?'https':'http'}/${HashHelper.md5(url.substr(5))}`);
-  if (await fs.exists(file)){
+  if (await fs.pathExists(file)){
     res.type('css').sendFile(file);
   } else {
-    let response = await request.get('https://fonts.googleapis.com' + url);
+    let response = await request.get('https://fonts.googleapis.com' + url, {resolveWithFullResponse: true});
     if (response.statusCode !== 200){
-      return res.status(response.statusCode).send(response.body);
+      return res.status(response.statusCode).type(response.headers['content-type']).send(response.body);
     }
+
     let style = ReplaceHelper.replaceBody(response.body);
     res.type('css').send(style);
-    if (! (await fs.exists(path.dirname(file)))){
-      await mkdirp(path.dirname(file));
-    }
-    await fs.writeFile(file, style);
+
+    await fs.outputFile(file, style);
   }
 });
 
 router.get('/s/*', async function (req, res) {
   let url = req.url;
   let file = path.join(publicDir, url);
-    if (await fs.exists(file)){
+    if (await fs.pathExists(file)){
       res.type('font').sendFile(file);
     } else {
-      let response = await request.get('https://fonts.gstatic.com' + url, {encoding: null});
+      let response = await request.get('https://fonts.gstatic.com' + url, {encoding: null, resolveWithFullResponse: true});
       if (response.statusCode !== 200){
-        return res.status(response.statusCode).send(response.body);
+        return res.status(response.statusCode).type(response.headers['content-type']).send(response.body);
       }
+
       let font = response.body;
       res.type('font').send(font);
-      if (! (await fs.exists(path.dirname(file)))){
-        await mkdirp(path.dirname(file));
-      }
-      await fs.writeFile(file, font);
+
+      await fs.outputFile(file, font);
     }
 });
 
 router.get('/ajax/*', async function (req, res) {
   let url = req.url;
   let file = path.join(publicDir, url);
-  if (await fs.exists(file)){
+  if (await fs.pathExists(file)){
     res.type('js').sendFile(file);
   } else {
-    let response = await request.get('https://ajax.googleapis.com' + url);
+    let response = await request.get('https://ajax.googleapis.com' + url, {resolveWithFullResponse: true});
     if (response.statusCode !== 200){
-      return res.status(response.statusCode).send(response.body);
+      return res.status(response.statusCode).type(response.headers['content-type']).send(response.body);
     }
+
     let font = response.body;
     res.type('js').send(font);
-    if (! (await fs.exists(path.dirname(file)))){
-      await mkdirp(path.dirname(file));
-    }
-    await fs.writeFile(file, font);
+
+    await fs.outputFile(file, font);
   }
 });
 
