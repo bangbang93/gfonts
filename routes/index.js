9ea53b0ba302ec78fbaf08dddfc96f3c2d91171c
bangbang93 <bangbang93@163.com>
2024-09-05 16:54:40 +0800

    feat: 给个能看的首页吧

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendStatus(200).end();
+  res.send(`
+    <script>
+    const baseUrl = location.origin
+    </script>
+    <h1>Google Fonts Proxy</h1>
+    <p>Usage:</p>
+    <ul>
+      <li><script> document.write(baseUrl) </script>/css?family=Roboto</li>
+      <li><script> document.write(baseUrl) </script>/css?family=Roboto:400,700</li>
+      <li><script> document.write(baseUrl) </script>/css?family=Roboto:400,700|Material+Icons</li>
+      <li><script> document.write(baseUrl) </script>/icon?family=Material+Icons</li>
+    </ul>
+    <hr>
+    <p>Source code: <a href="https://github.com/bangbang93/gfonts"> https://github.com/bangbang93/gfonts </a></p>
+  `);
 });
 
 module.exports = router;
